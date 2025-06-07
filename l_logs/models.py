86b6eab118b924d5e088197953e57fec77ea2435
RRedladeRR <kuznetsovroman775@gmail.com>
2025-06-07 17:29:28 +0300

    Added Entry model

--- l_logs/models.py
+++ l_logs/models.py
@@ -8,5 +8,24 @@ class Topic(models.Model):
     text = models.CharField(max_length=255, verbose_name="Topic name")
     date_added = models.DateTimeField(auto_now_add=True, verbose_name="Created")
 
+
+    class Meta:
+        verbose_name = "Topic"
+        verbose_name_plural = "Topics"
+
     def __str__(self):
         return self.text
+    
+class Entry(models.Model):
+    """Дописи за темами"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, verbose_name="Topic")
+    text = models.TextField(verbose_name="Text")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
