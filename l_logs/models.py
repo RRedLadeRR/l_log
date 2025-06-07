689d986b39537eb5b78dee407b9e95916a3c88e5
RRedladeRR <kuznetsovroman775@gmail.com>
2025-06-07 17:03:40 +0300

    Added Topic to admin site

--- l_logs/models.py
+++ l_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Topic that user learning"""
+
+    text = models.CharField(max_length=255, verbose_name="Topic name")
+    date_added = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+
+    def __str__(self):
+        return self.text
