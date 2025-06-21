afc1f7e0b76bea50b0d064f2f6de8ade8daf000a
RRedladeRR <kuznetsovroman775@gmail.com>
2025-06-21 15:52:45 +0300

    Added posts editing

--- l_logs/views.py
+++ l_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .forms import TopicForm, EntryForm
-from .models import Topic
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -57,3 +57,23 @@ def new_entry(request, topic_id):
         'form': form,
     }
     return render(request, 'l_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Editing existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('l_logs:topic', topic_id=topic.id)
+        
+    context = {
+        'entry': entry,
+        'topic': topic,
+        'form': form
+    }
+    return render(request, 'l_logs/edit_entry.html', context)
