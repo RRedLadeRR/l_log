627e8a1e2aff5397321444387de2fe59c1112a7f
RRedladeRR <kuznetsovroman775@gmail.com>
2025-06-21 18:07:03 +0300

    Added bootstrap navbar

--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     # My apps
     'l_logs',
     'accounts',
+    # Third party apps
+    'django_bootstrap5',
     # Django premade apps
     'django.contrib.admin',
     'django.contrib.auth',
